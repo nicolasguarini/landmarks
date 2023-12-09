b50d3931500b736fd6058bac977905f639911bae
Nicol Emanuele <nicolemanuele@Nicols-MacBook-Air.local>
2023-12-10 00:03:18 +0100

    Add location, persistence

--- src/main/java/bicocca2023/assignment3/Location.java
+++ src/main/java/bicocca2023/assignment3/Location.java
@@ -1,2 +1,42 @@
-package bicocca2023.assignment3;public class Location {
+package bicocca2023.assignment3;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Location {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private double latitude;
+    private double longitude;
+
+    // Costruttori, getter, setter e altri metodi se necessario
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public double getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(double latitude) {
+        this.latitude = latitude;
+    }
+
+    public double getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(double longitude) {
+        this.longitude = longitude;
+    }
 }
